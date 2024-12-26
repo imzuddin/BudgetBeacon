526b32eda78c20080a1fdf049fea30040c96950c
imzuddin <imzuddin@gmail.com>
2024-12-25 20:31:39 -0500

    feat: initialized fastapi w/ routers

---
+++ api/code/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+
+import os
+import asyncio
+
+
+from util.dynamic_cors_middleware import DynamicCORSMiddleware
+from router import authentication, budget, transactions, reporting
+
+api_path_prefix = os.environ.get("API_PATH_PREFIX", "/api/v1")
+
+app = FastAPI()
+
+origins = []
+app.add_middleware(
+    DynamicCORSMiddleware,
+    allow_origins=[],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(authentication.router, prefix=api_path_prefix)
+app.include_router(budget.router, prefix=api_path_prefix)
+app.include_router(transactions.router, prefix=api_path_prefix)
+app.include_router(reporting.router, prefix=api_path_prefix)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
