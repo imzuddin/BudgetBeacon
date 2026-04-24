4fc7a4f86e84aedf4549dc64caa6c15db4bbae6f
temp <temp>
2026-04-23 22:57:30 -0400

    create settings files to hold env vars

---
+++ config/settings.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_FILE = BASE_DIR / "config" /".env"
+
+print(ENV_FILE)
+class Settings(BaseSettings):
+    api_name: str = "Budget Beacon API"
+    
+    is_dev: bool
+    debug: bool 
+    postgres_host: str 
+    postgres_port: int 
+    postgres_db: str
+    postgres_user: str
+    postgres_password: str 
+
+    model_config = SettingsConfigDict(
+        env_file=ENV_FILE,
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
+    @property
+    def database_url(self) -> str:
+        return (
+            f"postgresql+psycopg://"
+            f"{self.postgres_user}:{self.postgres_password}"
+            f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+        )
+    
+settings = Settings()
